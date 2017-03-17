2c3b4f7d0e1395a2383620ad8c9ce84691ca8203
CfNb <ppimac3@PPiMac3s-iMac.local>
2017-03-17 15:31:43 -0500

    iniitial commit

---
+++ js/main.js
@@ -0,0 +1,83 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, window, location, CSInterface, SystemPath, themeManager*/
+
+$(document).ready(function () {
+	'use strict';
+    
+    var csInterface = new CSInterface();
+    
+    
+    
+    function init() {
+        themeManager.init();
+    }
+    
+    function closeNotifier(divID) {
+        $('#' + divID).hide();
+        $('#' + divID + 'Text').text('');
+    }
+    
+    function onDocSaved(event) {
+        console.log(Date() + ' doc saved');
+        console.log(event.data);
+    }
+    
+    function onDocDeactivated(event) {
+
+    }
+        
+    function onDocActivated(event) {
+        //disable if doc not open
+        var name = $(event.data).find("name").text();
+        if (name === '') {
+            $('button').prop('disabled', true);
+            $("#controls").addClass("disabled");
+            return;
+        }
+        $('button').prop('disabled', false);
+        $("#controls").removeClass("disabled");
+    }
+    
+    //////////////////////////////////
+    
+    $('#deny').click(function () {
+        
+        closeNotifier('confirmation');
+    });
+        
+    $('#confirm').click(function () {
+        console.log('confirmed');
+    });
+     
+    $('#notifierClose').click(function () {
+        closeNotifier('notifier');
+    });
+    
+    $('#retryButton').click(function () {
+        console.log(Date() + ' Retry');
+
+        closeNotifier('retry');
+    });
+
+    
+    //////////////////////////////////
+    
+    //themeManager:
+    init();
+    
+    //listen for ai document deactivations
+    csInterface.addEventListener("documentAfterDeactivate", onDocDeactivated);
+    
+    //listen for ai document activations
+    csInterface.addEventListener("documentAfterActivate", onDocActivated);
+    
+    //listen for ai document save
+    //csInterface.addEventListener("documentAfterSave", onDocSaved);
+    
+    /*
+    
+    csInterface.evalScript('docURL()');
+    
+    */
+    
+});
---
+++ jsx/hostscript.jsx
@@ -0,0 +1,63 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global $, Folder*/
+
+function addSlit(orientation, cutTrue, whiteTrue) {
+    //get selected path
+    
+    //if path not valid, return
+    
+    //get dims, calc slit line location by dims and orientation
+    
+    //check slit layer exists, make if needed
+    //if layer exitst, delete existing slit marks
+    
+    //make slit lines and press marks, if whiteTrue then 5indigo dashes
+    
+    //make slit width text
+    
+    //check finishing marks layer exists, make if needed
+    //if layer existed, delete existing cut marks
+    
+    //if cutTrue, create cut marks
+}
+
+function addTear(orientation) {
+    // ask for distance from edge
+    
+    //check finishing marks layer exists, make if needed
+    //if layer existed, delete existing tear marks
+    
+    //check info layer exists, make if needed
+    //if layer existed, delete existing tear marks
+    
+    // create marks on finsishing layer
+    
+    // create marks on info layer
+}
+
+function add1Fold(orientation) {
+    // ask for distance from top edge
+    
+    //check finishing marks layer exists, make if needed
+    //if layer existed, delete existing fold marks
+    
+    // create marks on finsishing layer
+}
+
+function add2Fold(orientation) {
+    // ask for distance from top edge
+    
+    //check finishing marks layer exists, make if needed
+    //if layer existed, delete existing fold marks
+    
+    // create marks on finsishing layer, one set from 1st edge, 2nd set from opposite edge
+}
+
+function add3Fold(orientation) {
+    // for quad seal bag
+}
+
+function add4Fold(orientation) {
+    // for side gusset bag
+}
+
